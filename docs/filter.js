620ca845b6154ad880073ffa4731fc390bae9416
everythingdrive <36702866+everythingdrive@users.noreply.github.com>
2018-02-22 18:41:40 -0500

    Add files via upload

---
+++ docs/filter.js
@@ -0,0 +1,9 @@
+//GAME FILTERS
+function showAdmin() {
+  document.getElementById('adminneeded').style.display="inline-block";
+  document.getElementById('remove-filter-btn').style.display="inline-block";
+}
+function hideAdmin() {
+  document.getElementById('adminneeded').style.display="none";
+  document.getElementById('remove-filter-btn').style.display="none";
+}
