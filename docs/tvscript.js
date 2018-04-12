e75f19defa35feb2b444fd02109939e14ae30ed4
everythingdrive <36702866+everythingdrive@users.noreply.github.com>
2018-04-12 16:42:15 -0400

    Add files via upload

---
+++ docs/tvscript.js
@@ -0,0 +1,16 @@
+function blackmirror() {
+document.getElementById('blackmirrorep').style.display="block";
+document.getElementById('hidebmbtn').style.display="inline-block";
+}
+function hideblackmirror() {
+document.getElementById('blackmirrorep').style.display="none";
+document.getElementById('hidebmbtn').style.display="none";
+}
+function heathers() {
+document.getElementById('heathersep').style.display="block";
+document.getElementById('hidehtbtn').style.display="inline-block";
+}
+function hideheathers() {
+document.getElementById('heathersep').style.display="none";
+document.getElementById('hidehtbtn').style.display="none";
+}
