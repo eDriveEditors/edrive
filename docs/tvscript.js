e6b3af041ae46a029d24783987b05abd3aae5481
everythingdrive <36702866+everythingdrive@users.noreply.github.com>
2018-04-16 13:31:40 -0400

    Add files via upload

--- docs/tvscript.js
+++ docs/tvscript.js
@@ -14,3 +14,11 @@ function hideheathers() {
 document.getElementById('heathersep').style.display="none";
 document.getElementById('hidehtbtn').style.display="none";
 }
+function heroesreborn() {
+document.getElementById('heroesrebornep').style.display="block";
+document.getElementById('hidehrbtn').style.display="inline-block";
+}
+function hideheroesreborn() {
+document.getElementById('heroesrebornep').style.display="none";
+document.getElementById('hidehrbtn').style.display="none";
+}
